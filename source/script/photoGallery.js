816cb081030ff445904e5b61e3b94ea964f3d4cb
nadsv <nadsv@newmail.ru>
2016-02-12 10:02:49 +0300

    Add photoGallery

---
+++ source/script/photoGallery.js
@@ -0,0 +1,84 @@
+//Send form
+(function sendForm() {
+	if (!("FormData" in window)) {
+		return;
+		}
+
+	var form = document.querySelector(".recall-form");
+	if (form) {
+		var queue = [];
+		var template = document.querySelector("#photo-template").innerHTML; 
+
+		if ("FileReader" in window) {
+        	var gallery = document.querySelector(".recall-form__gallery"); 
+
+			form.querySelector("#upload-photo").addEventListener("change", function() {
+				var files = this.files;
+ 				for (var i = 0; i < files.length; i++) {
+ 					preview(files[i]);
+ 				}
+ 				this.value = "";
+			});
+
+//Preview
+			function preview(file) {
+				if (file.type.match(/image.*/)) {
+
+					var reader = new FileReader();
+
+					reader.addEventListener("load", function(event) {
+						var html = Mustache.render(template, {
+ 							"src": event.target.result,
+ 							"photo-name": file.name
+ 							});
+
+						var div = document.createElement("div");
+						div.classList.add("recall-form__loaded-photo");
+ 						div.innerHTML = html;
+						gallery.appendChild(div); 
+						queue.push({file: file, div: div});
+						div.querySelector(".recall-form__delete-photo").addEventListener("click",
+						function(event) {
+							event.preventDefault();
+ 							removePreview(div);
+						});
+ 					});
+
+ 					reader.readAsDataURL(file);
+ 				}
+			}
+
+			function removePreview(div) {
+ 				queue = queue.filter(function(element) {
+ 				return element.div != div;
+ 				});
+ 				div.parentNode.removeChild(div);
+			}		
+		}
+//Submit
+		form.addEventListener("submit", function(event) {
+			event.preventDefault();
+			var data = new FormData(form);
+
+			queue.forEach(function(element) {
+ 				data.append("images", element.file);
+ 			});
+
+			request(data, function(response) {
+			console.log(response);
+			});
+		});
+	}
+
+	function request(data, fn) {
+		var xhr = new XMLHttpRequest();
+		var time = (new Date()).getTime();
+		xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time);
+		xhr.addEventListener("readystatechange", function() {
+		if (xhr.readyState == 4) {
+			fn(xhr.responseText);
+		}
+	});
+		xhr.send(data);
+	}
+}) ();

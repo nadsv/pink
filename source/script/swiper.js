93910e209a3e6c17f5f8717815e9cc027d1db463
nadsv <nadsv@newmail.ru>
2016-02-23 13:54:24 +0400

    Some small bugs was fixed

--- source/script/swiper.js
+++ source/script/swiper.js
@@ -1,14 +1,15 @@
 //Slider Initialization
 (function mainMenuAnimate() {
 	"use strict";
-	
-	 var swiper = new Swiper('.review', {
-        pagination: '.review__pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 30,
-        loop: true
-    });
-
+	 
+     if (document.querySelector('.review')) {
+	    var swiper = new Swiper('.review', {
+            pagination: '.review__pagination',
+            paginationClickable: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            spaceBetween: 30,
+            loop: true
+        });
+    }
 })();
